a25bb27817d12a76fb7062797829c976b83fcf1b
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-22 09:59:28 -0400

    Update app_requests.py

--- app_requests.py
+++ app_requests.py
@@ -1,1 +1,32 @@
-import request
+import asyncio
+import time
+import requests
+from pyrogram import Client , filters
+from pyrogram.types import Message, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+import os
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+#from json import loads,dumps
+from pyrogram.types import ForceReply
+from pyrogram.handlers import MessageHandler
+
+api_id = 9910861
+api_hash = "86e927460a8998ba6d84e9c13acfda95"
+bot_token = "5871277082:AAFsEc0clhaeJ0wokJVfGF0_P3P0385Sb0M"
+bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
+
+@bot.on_message(filters.command('add') & filters.private & filters.incoming)
+async def add(bot, message):
+    send = message.reply
+    username = message.from_user.username
+
+
+
+
+
+
+
+
+bot.start()
+bot.send_message(5416296262,'**BoT Iniciado**')
+bot.loop.run_forever()

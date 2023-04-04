752011d56787cf507f810a1a34dc40ddb5cd0c51
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-04-04 11:37:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def download_file():
             total_size_in_bytes = int(response.headers.get('content-length', 0))
             block_size = 1024
             progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-            def generate():
+          #  def generate():
           #      with open('/storage/emulated/0/Download/'+filename, 'wb') as file:
           #          for data in response.iter_content(block_size):
           #              progress_bar.update(len(data))
@@ -45,6 +45,6 @@ def download_file():
                 'Content-Type': 'application/octet-stream',
                 'Cache-Control': 'no-cache'
             }
-            return Response(generate(), headers=headers)
+            return Response(headers=headers)
     else:
         return render_template('index.html')
